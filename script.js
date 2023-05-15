69e96b796ff94cab2e39f2766c4cc5afd42697e9
ujjwal <ujjwalsharma1772003@gmail.com>
2023-05-16 04:14:35 +0530

    moving l-r luffy

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ document.onkeydown = function(e){
             luffy.classList.remove('animateLuffy')
         }, 700);
     }
+    if(e.keyCode==39){
+        luffy = document.querySelector(".luffy")
+        luffyX = parseInt(window.getComputedStyle(luffy, null).getPropertyValue('left'))
+        luffy.style.left = luffyX + 112 + "px"
+    }
+    if(e.keyCode==37){
+        luffy = document.querySelector(".luffy")
+        luffyX = parseInt(window.getComputedStyle(luffy, null).getPropertyValue('left'))
+        luffy.style.left = luffyX - 112 + "px"
+    }
 }
 
 setInterval(() => {
@@ -14,16 +24,17 @@ setInterval(() => {
     gameOver = document.querySelector('.gameOver')
     obstacle = document.querySelector('.obstacle')
 
-     lx = window.getComputedStyle(luffy, null).getPropertyValue('left')
-     ly = window.getComputedStyle(luffy, null).getPropertyValue('top')
+     lx = parseInt(window.getComputedStyle(luffy, null).getPropertyValue('left'))
+     ly = parseInt(window.getComputedStyle(luffy, null).getPropertyValue('top'))
 
-     ox = window.getComputedStyle(obstacle, null).getPropertyValue('left')
-     oy = window.getComputedStyle(obstacle, null).getPropertyValue('top')
+     ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'))
+     oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'))
 
-     offsetX = Math.abs(dx-ox)
-     offsetY = Math.abs(dy-oy)
-    if(offsetX< 93 && offsetY<52){
+     offsetX = Math.abs(lx-ox)
+     offsetY = Math.abs(ly-oy)
+     console.log(offsetX, offsetY)
+    if(offsetX< 53 && offsetY<32){
         gameOver.style.visibility = 'visible'
-        
+        obstacle.classList.remove('obstacleAni')
     }
 }, 100);
